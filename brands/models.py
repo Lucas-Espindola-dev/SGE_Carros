21e9b80c2b8ccdfbc2f239ffd9fe283c1621ee9d
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-10-15 16:42:43 -0300

    Adding ordeting and str

--- brands/models.py
+++ brands/models.py
@@ -7,3 +7,9 @@ class Brand(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
