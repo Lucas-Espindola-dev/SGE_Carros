6e0c58353430e855a3691ca7206c26724c1e0b88
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-10-16 11:02:51 -0300

    Adding choices to colors

--- cars/models.py
+++ cars/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from brands.models import Brand
 
 
+COLORS = (
+    ('RED', 'Red'),
+    ('WHITE', 'White'),
+    ('GRAY', 'Gray'),
+    ('SILVER', 'Silver'),
+)
+
+
 class Car(models.Model):
     model = models.CharField(max_length=250)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='cars')
     factory_year = models.IntegerField()
     model_year = models.IntegerField()
     plate = models.CharField(max_length=10)
-    color = models.CharField(max_length=30)
+    color = models.CharField(max_length=30, choices=COLORS,)
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
