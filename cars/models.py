3a8c2eac77bc795b9a0602f1f34d975c07b7b3bb
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-10-16 10:50:04 -0300

    Creating the cars model

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from brands.models import Brand
 
-# Create your models here.
+
+class Car(models.Model):
+    model = models.CharField(max_length=250)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='cars')
+    factory_year = models.IntegerField()
+    model_year = models.IntegerField()
+    plate = models.CharField(max_length=10)
+    color = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
