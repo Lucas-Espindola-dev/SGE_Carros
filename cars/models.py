151e2909141df26b2998b8822025477ef99c4406
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-10-18 16:53:01 -0300

    Creating class meta and __str__

--- cars/models.py
+++ cars/models.py
@@ -20,3 +20,9 @@ class Car(models.Model):
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'f {self.model} - {self.color} - {self.model_year} '
