fd7d9a18f44070fb61a0588a7360f46fe292852a
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-10-30 15:12:50 -0300

    Changing the template name

--- brands/views.py
+++ brands/views.py
@@ -4,5 +4,5 @@ from . import models
 
 class BrandListView(ListView):
     model = models.Brand
-    template_name = 'brands_list.html'
+    template_name = 'brand_list.html'
     context_object_name = 'brands'
